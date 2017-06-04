21ec0cd2883d54b67a91943fdae53dff7dc35a6a
Samantha Bretous <sam@samanthabretous.com>
2017-06-04 11:18:47 -0400

    slides

---
+++ presentation/index.js
@@ -0,0 +1,172 @@
+// Import React
+import React from "react";
+
+// Import Spectacle Core tags
+import {
+  Appear,
+  BlockQuote,
+  Cite,
+  Deck,
+  Fill,
+  Fit,
+  Heading,
+  Image,
+  Layout,
+  ListItem,
+  List,
+  Quote,
+  Slide,
+  Text
+} from "spectacle";
+
+// Import image preloader util
+import preloader from "spectacle/lib/utils/preloader";
+
+// Import theme
+import createTheme from "spectacle/lib/themes/default";
+
+// Require CSS
+require("normalize.css");
+require("spectacle/lib/themes/default/index.css");
+
+
+const images = {
+  sam: require("../assets/sam.jpg"),
+  openOffice: require("../assets/openOffice.jpg"),
+  scream: require("../assets/scream.gif"),
+  sut: require("../assets/shut-up-tom-logo.png"),
+  cent: require("../assets/cent.jpg"),
+  d1: require("../assets/diagram-sup1.png"),
+  d2: require("../assets/diagram-sup2.png"),
+  thanks: require("../assets/diagram-sup3.png"),
+  markdown: require("../assets/markdown.png")
+};
+
+preloader(images);
+
+const theme = createTheme({
+  white: "white",
+  black: "#1F2022",
+  purple: "#CC67C6",
+  blue: "#142553",
+  lightblue: "#85D8E2",
+}, {
+  primary: "Montserrat",
+  secondary: "Helvetica"
+});
+
+// tranisiton types -- zoom fade slide
+
+export default class Presentation extends React.Component {
+  render() {
+    return (
+      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
+        <Slide transition={["zoom"]} bgColor="purple" textColor="blue">
+          <Heading size={5} fit caps textColor="blue" lineHeight={1}>Hi, I'm Sam
+          </Heading>
+          <Heading size={5} fit caps textColor="blue" lineHeight={1}>@samanthabretous</Heading>
+            <Image src={images.sam} width={300}/>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="white">
+          <Heading size={2} fit caps lineHeight={1} textColor="lightblue">Open Office</Heading>
+          <Image src={images.openOffice} width={400}/>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="white">
+          <Image src={images.scream} width={600}/>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="blue">
+          <Image src={images.d1} width={700}/>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="blue">
+          <Heading size={1} fit caps lineHeight={1} textColor="white">
+            DEMO TIME
+          </Heading>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="blue" textColor="white">
+          <Layout>
+            <Fit>
+              <BlockQuote>
+                <Quote>When in doubt, use Javascript.</Quote>
+                <Appear>
+                  <Cite>Curtis Jackson</Cite>
+                </Appear>
+              </BlockQuote>
+            </Fit>
+            <Fit>
+              <Appear>
+                <Image src={images.cent} width={300}/>
+              </Appear>
+            </Fit>
+          </Layout>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="blue" textColor="white">
+          <Heading size={4} textColor="purple" caps>Front End</Heading>
+          <List>
+            <ListItem>Electron</ListItem>
+            <ListItem>Handlebars</ListItem>
+            <ListItem>Typescript</ListItem>
+            <ListItem>Gulp</ListItem>
+            <ListItem>CSS Animations</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="blue" textColor="white">
+          <Heading size={4} textColor="purple" caps>Hardware</Heading>
+          <List>
+            <ListItem>Johnny Five JS</ListItem>
+            <ListItem>Firmata</ListItem>
+            <ListItem>Ardunio</ListItem>
+            <ListItem>Wires</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="blue" textColor="white">
+          <Heading size={4} textColor="purple" caps>Back End</Heading>
+          <List>
+            <ListItem>
+              <Text textColor="lightblue">Slack API</Text>
+              <Text textColor="white">- Incoming webhooks and slash commands</Text>
+            </ListItem>
+            <ListItem>
+              <Text textColor="lightblue">Express JS</Text>
+              <Text textColor="white">- Server-side Rendering and API Routes</Text>
+            </ListItem>
+            <ListItem>
+              <Text textColor="lightblue">Superagent</Text>
+              <Text textColor="white">- RESTful Communication between Microservices</Text>
+            </ListItem>
+            <ListItem>
+              <Text textColor="lightblue">Socket.io</Text>
+              <Text textColor="white">- Push Communication to Client</Text>
+            </ListItem>
+            <ListItem>
+              <Text textColor="lightblue">Mongo DB</Text>
+              <Text textColor="white">Database Storage</Text>
+            </ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="blue">
+          <Image src={images.d2} width={900}/>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="blue" textColor="white">
+          <Heading size={6} textColor="purple" caps>Next Steps</Heading>
+          <List>
+            <Appear>
+              <ListItem>Finish DOCKER networking and upload to AWS EC2</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>Add More Slack Commands</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>Graphs with SVG animations</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>Automatic scheduling for sound levels</ListItem>
+            </Appear>
+          </List>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="blue">
+          <Image src={images.thanks} width={900}/>
+        </Slide>
+      </Deck>
+    );
+  }
+}
